42fdf015578be23653f99a2f4469918b48aebf26
Oketa Fred <oketafred@gmail.com>
2019-05-23 17:37:52 +0300

    Added Search Functionality on the Index Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,10 @@
-<h1>404 Error Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>404 Error Page</title>
+</head>
+<body>
+	<h1>404 Error Page</h1>
+</body>
+</html>
